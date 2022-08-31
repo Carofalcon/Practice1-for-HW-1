2eccf65701150c988d20543352a7d6ceec15acda
17865 <17865@LAPTOP-KLVLVNQK>
2022-08-31 19:22:24 -0400

    Add project files.

---
+++ Practice1 for HW 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Practice1_for_HW_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("   XXXXXX");
+            WriteLine(" X        X");
+            WriteLine("X   STOP   X");
+            WriteLine(" X        X");
+            WriteLine("   XXXXXX");
+            WriteLine("      X");
+            WriteLine("      X");
+            WriteLine("      X");
+        }
+      
+        
+        
+    }
+}
